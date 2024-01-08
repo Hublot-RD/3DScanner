2e5473c0c5415aae703011595761bd1a2f9c7bf1
vphilippoz <82140967+vphilippoz@users.noreply.github.com>
2024-01-08 16:16:25 +0100

    Launch with wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,10 @@
 # from app import create_app
 from app import app, socketio
-print('hey')
-print('name:', __name__)
-
-
-# Create the Flask application
-# app, socketio = create_app()
 
 if __name__ == '__main__':
     # Run the Flask-SocketIO application for development
+    print("Running in debug mode")
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+else:
+    # Run the Flask-SocketIO application for production
+    print("Running in production mode")

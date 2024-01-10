e9a00f03a2cb63ef8c5cc3136f46ae149781b071
RaspberryPi <vphilippoz@gmail.com>
2024-01-10 13:30:31 +0100

    More minor changes

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from app import app, socketio
 if __name__ == '__main__':
     # Run the Flask-SocketIO application for development
     print("Running in debug mode")
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='127.0.0.1', port=5000, allow_unsafe_werkzeug=True)
 else:
     # Run the Flask-SocketIO application for production
     print("Running in production mode")
